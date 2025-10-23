585860c26cb723604cc13951bc6778e461b3a7a9
Million Dollar Blueprint <info@milliondollarblueprint.ai>
2025-10-22 22:57:39 -0400

    Use player.vimeo.com URLs with hash parameter for embedding

--- app/api/update-video-urls/route.ts
+++ app/api/update-video-urls/route.ts
@@ -51,18 +51,18 @@ export async function GET() {
     const module = course.modules[0]
     const lessons = module.lessons
 
-    // Authenticated Vimeo URLs with privacy hashes (bypass domain restrictions)
+    // Vimeo player URLs with privacy hash parameter (for ReactPlayer embedding)
     const videoUrls = [
-      'https://vimeo.com/1129374864/8462d84da1',
-      'https://vimeo.com/1129414459/c3afffad25',
-      'https://vimeo.com/1129414864/272f58a324',
-      'https://vimeo.com/1129415335/47efd8a1aa',
-      'https://vimeo.com/1129415686/b37c86e62c',
-      'https://vimeo.com/1129415885/7bba6f24bd',
-      'https://vimeo.com/1129416243/657cd05863',
-      'https://vimeo.com/1129417480/946e450263',
-      'https://vimeo.com/1129417841/f4d6644de9',
-      'https://vimeo.com/1129418207/8f8000ae4f'
+      'https://player.vimeo.com/video/1129374864?h=8462d84da1',
+      'https://player.vimeo.com/video/1129414459?h=c3afffad25',
+      'https://player.vimeo.com/video/1129414864?h=272f58a324',
+      'https://player.vimeo.com/video/1129415335?h=47efd8a1aa',
+      'https://player.vimeo.com/video/1129415686?h=b37c86e62c',
+      'https://player.vimeo.com/video/1129415885?h=7bba6f24bd',
+      'https://player.vimeo.com/video/1129416243?h=657cd05863',
+      'https://player.vimeo.com/video/1129417480?h=946e450263',
+      'https://player.vimeo.com/video/1129417841?h=f4d6644de9',
+      'https://player.vimeo.com/video/1129418207?h=8f8000ae4f'
     ]
 
     // Update each lesson with correct video URL
